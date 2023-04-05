0e70b987ef7c60a8ed5d53b99809f986f5989122
Rhys Webb <rtaddley@gmail.com>
2023-04-05 20:37:55 +1200

    Updates for saving
    Updates to save and load features

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MainManager : MonoBehaviour
+{
+    public static MainManager Instance;
+
+    // Variables
+    public Color teamColour;
+
+    private void Awake()
+    {
+        // start of new code
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        // end of new code
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        LoadColor();
+    }
+    [System.Serializable]
+    class SaveData
+    {
+        public Color teamColour;
+    } // Class
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.teamColour = teamColour;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "saveFile.json", json);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "saveFile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            teamColour = data.teamColour;
+        }
+    }
+} // Class
